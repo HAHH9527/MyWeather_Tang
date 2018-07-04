f821beaf647487fff72f9422a0b1ef856fe421d9
9527Number <1024830255@qq.com>
2018-07-04 11:53:07 +0800

    0704 NavigationView 点击事件

--- app/src/main/java/com/example/a10248/myweather_tang/activity/WeatherActivity.java
+++ app/src/main/java/com/example/a10248/myweather_tang/activity/WeatherActivity.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -36,6 +38,7 @@ import com.example.a10248.myweather_tang.util.MyMessageType;
 
 import org.litepal.tablemanager.Connector;
 
+import java.io.File;
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -51,12 +54,18 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
     private String city = "";
     private AMapLocation aMapLocation;
     private boolean autoLocation = true;
+    //BGM有关
+    private int nowBgmId = R.raw.forest;
 
     //布局
     private SwipeRefreshLayout swipe_refresh;
     private DrawerLayout drawable_layout;
     private ImageView bing_pic_img;
     private NavigationView navView;
+
+    //侧滑菜单
+    private View headerView;
+    private TextView tv_user_name;
 
     //实时天气
     private TextView tv_temperature;
@@ -76,6 +85,8 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
     private TextView tv_forecast3_date;
     private TextView tv_forecast3_weather;
     private TextView tv_forecast3_temperature;
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,6 +114,9 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
         drawable_layout = (DrawerLayout) findViewById(R.id.drawable_layout);
         navView = (NavigationView) findViewById(R.id.navView);
         navView.setNavigationItemSelectedListener(this);
+        headerView = navView.getHeaderView(0);
+        tv_user_name = (TextView) headerView.findViewById(R.id.tv_user_name);
+        tv_user_name.setOnClickListener(this);
 
         tv_temperature = (TextView) findViewById(R.id.tv_temperature);
         tv_temperatureFell = (TextView) findViewById(R.id.tv_temperatureFell);
@@ -123,53 +137,30 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     /**
-     * 监听Handler
+     * 设置当前BGM
+     *
+     * @param id
      */
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case MyMessageType.Return_City_Message:
-                    aMapLocation = (AMapLocation) msg.obj;
-                    Toast.makeText(getApplicationContext(), "当前城市:" + aMapLocation.getCity(), Toast.LENGTH_SHORT).show();
-                    tv_address.setText(aMapLocation.getCity());
-                    getWeather(aMapLocation.getLongitude() + "," + aMapLocation.getLatitude());
-                    break;
-                case MyMessageType.Return_WeatherNow_Message:
-                    Log.i("weather in main", msg.obj.toString());
-                    MyNow myNow = (MyNow) msg.obj;
-                    tv_temperature.setText(myNow.getTemperature());
-                    tv_temperatureFell.setText(myNow.getTemperature_feel());
-                    tv_address.setText(myNow.getLocation());
-                    tv_weatherText.setText(myNow.getWeather());
-                    tv_windir.setText(myNow.getWind_dir() + " " + myNow.getWind_sc() + "级");
-                    tv_pcpn.setText(myNow.getPcpn());
-                    swipe_refresh.setRefreshing(false);
-                    break;
-                case MyMessageType.Return_WeatherNow_Message_Fail:
-                    Toast.makeText(getApplicationContext(), "当前天气查询出错", Toast.LENGTH_SHORT).show();
-                    swipe_refresh.setRefreshing(false);
-                    break;
-                case MyMessageType.Return_WeatherForecast_Message:
-                    MyForecast myForecast = (MyForecast) msg.obj;
-                    tv_forecast1_date.setText(myForecast.getDate1());
-                    tv_forecast1_weather.setText(myForecast.getCond1());
-                    tv_forecast1_temperature.setText(myForecast.getTmp1());
-                    tv_forecast2_date.setText(myForecast.getDate2());
-                    tv_forecast2_weather.setText(myForecast.getCond2());
-                    tv_forecast2_temperature.setText(myForecast.getTmp2());
-                    tv_forecast3_date.setText(myForecast.getDate3());
-                    tv_forecast3_weather.setText(myForecast.getCond3());
-                    tv_forecast3_temperature.setText(myForecast.getTmp3());
-                    swipe_refresh.setRefreshing(false);
-                    break;
-                case MyMessageType.Return_WeatherForecast_Message_Fail:
-                    Toast.makeText(getApplicationContext(), "未来天气查询出错", Toast.LENGTH_SHORT).show();
-                    swipe_refresh.setRefreshing(false);
-                    break;
-            }
-        }
-    };
+    private void setNowBgmId(int id) {
+        nowBgmId = id;
+    }
+
+    /**
+     * 播放BGM
+     */
+    private void playBGM() {
+        if (mediaPlayer != null) mediaPlayer.stop();
+        mediaPlayer = MediaPlayer.create(this, nowBgmId);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+    }
+
+    /**
+     * 停止BGM
+     */
+    private void stopBGM() {
+        mediaPlayer.stop();
+    }
 
     /**
      * 获得城市
@@ -232,32 +223,151 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
     /**
      * 设置城市
      */
-    public void setCityDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(WeatherActivity.this);
-        builder.setTitle("请输入城市");
+    private void displaySetCityDialog() {
+        AlertDialog dialogSetCity = new AlertDialog.Builder(WeatherActivity.this).create();
+        dialogSetCity.setTitle("请输入城市");
         //    通过LayoutInflater来加载一个xml的布局文件作为一个View对象
         View view = LayoutInflater.from(WeatherActivity.this).inflate(R.layout.dialog_select_city, null);
         //    设置我们自己定义的布局文件作为弹出框的Content
-        builder.setView(view);
+        dialogSetCity.setView(view);
 
         final EditText edt_city = (EditText) view.findViewById(R.id.edt_city);
 
-        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+        dialogSetCity.setButton(AlertDialog.BUTTON_POSITIVE, "确定", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
+            public void onClick(DialogInterface dialogInterface, int i) {
                 city = edt_city.getText().toString();
                 autoLocation = false;
                 updateWeather();
+                dialogInterface.dismiss();
             }
         });
-        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
 
+        dialogSetCity.setButton(AlertDialog.BUTTON_NEGATIVE, "取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
             }
         });
-        builder.show();
+        dialogSetCity.show();
     }
+
+    /**
+     * 登录
+     */
+    private void displayLoginDialog() {
+        AlertDialog dialogLogin = new AlertDialog.Builder(this).create();//创建对话框
+//        dialog.setIcon(R.mipmap.ic_launcher);//设置对话框icon
+        dialogLogin.setTitle("欢迎使用和天气");//设置对话框标题
+        //    通过LayoutInflater来加载一个xml的布局文件作为一个View对象
+        View view = LayoutInflater.from(WeatherActivity.this).inflate(R.layout.dialog_login, null);
+        //    设置我们自己定义的布局文件作为弹出框的Content
+        dialogLogin.setView(view);
+
+        //分别设置三个button
+        dialogLogin.setButton(DialogInterface.BUTTON_POSITIVE, "确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();//关闭对话框
+            }
+        });
+
+        dialogLogin.setButton(DialogInterface.BUTTON_NEGATIVE, "取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();//关闭对话框
+            }
+        });
+
+        dialogLogin.setButton(DialogInterface.BUTTON_NEUTRAL, "注册", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+
+                dialogInterface.dismiss();
+                displayRegisterDialog();
+            }
+        });
+        dialogLogin.show();//显示对话框
+    }
+
+    /**
+     * 注册
+     */
+    private void displayRegisterDialog() {
+        AlertDialog dialogRegister = new AlertDialog.Builder(WeatherActivity.this).create();
+        dialogRegister.setTitle("注册");
+        //    通过LayoutInflater来加载一个xml的布局文件作为一个View对象
+        View view = LayoutInflater.from(WeatherActivity.this).inflate(R.layout.dialog_register, null);
+        //    设置我们自己定义的布局文件作为弹出框的Content
+        dialogRegister.setView(view);
+
+        dialogRegister.setButton(AlertDialog.BUTTON_POSITIVE, "确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                dialogInterface.dismiss();
+            }
+        });
+
+
+        dialogRegister.setButton(AlertDialog.BUTTON_NEGATIVE, "取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialogRegister.show();
+    }
+
+    /**
+     * 监听Handler
+     */
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MyMessageType.Return_City_Message:
+                    aMapLocation = (AMapLocation) msg.obj;
+                    Toast.makeText(getApplicationContext(), "当前城市:" + aMapLocation.getCity(), Toast.LENGTH_SHORT).show();
+                    tv_address.setText(aMapLocation.getCity());
+                    getWeather(aMapLocation.getLongitude() + "," + aMapLocation.getLatitude());
+                    break;
+                case MyMessageType.Return_WeatherNow_Message:
+                    Log.i("weather in main", msg.obj.toString());
+                    MyNow myNow = (MyNow) msg.obj;
+                    tv_temperature.setText(myNow.getTemperature());
+                    tv_temperatureFell.setText(myNow.getTemperature_feel());
+                    tv_address.setText(myNow.getLocation());
+                    tv_weatherText.setText(myNow.getWeather());
+                    tv_windir.setText(myNow.getWind_dir() + " " + myNow.getWind_sc() + "级");
+                    tv_pcpn.setText(myNow.getPcpn());
+                    swipe_refresh.setRefreshing(false);
+                    break;
+                case MyMessageType.Return_WeatherNow_Message_Fail:
+                    Toast.makeText(getApplicationContext(), "当前天气查询出错", Toast.LENGTH_SHORT).show();
+                    swipe_refresh.setRefreshing(false);
+                    break;
+                case MyMessageType.Return_WeatherForecast_Message:
+                    MyForecast myForecast = (MyForecast) msg.obj;
+                    tv_forecast1_date.setText(myForecast.getDate1());
+                    tv_forecast1_weather.setText(myForecast.getCond1());
+                    tv_forecast1_temperature.setText(myForecast.getTmp1());
+                    tv_forecast2_date.setText(myForecast.getDate2());
+                    tv_forecast2_weather.setText(myForecast.getCond2());
+                    tv_forecast2_temperature.setText(myForecast.getTmp2());
+                    tv_forecast3_date.setText(myForecast.getDate3());
+                    tv_forecast3_weather.setText(myForecast.getCond3());
+                    tv_forecast3_temperature.setText(myForecast.getTmp3());
+                    swipe_refresh.setRefreshing(false);
+                    break;
+                case MyMessageType.Return_WeatherForecast_Message_Fail:
+                    Toast.makeText(getApplicationContext(), "未来天气查询出错", Toast.LENGTH_SHORT).show();
+                    swipe_refresh.setRefreshing(false);
+                    break;
+            }
+        }
+    };
 
     /**
      * 监听点击
@@ -267,7 +377,11 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
+            case R.id.tv_user_name:
+                Log.i("点击", "tv_user_name");
+                displayLoginDialog();
+                drawable_layout.closeDrawer(Gravity.LEFT);
+                break;
         }
     }
 
@@ -296,10 +410,33 @@ public class WeatherActivity extends AppCompatActivity implements View.OnClickLi
                 updateWeather();
                 break;
             case R.id.nav_item_setCity:
-                setCityDialog();
+                displaySetCityDialog();
                 break;
-            case R.id.bgm_play:
-
+            case R.id.bgm_play_pause:
+                playBGM();
+                break;
+            case R.id.bgm_stop:
+                stopBGM();
+                break;
+            case R.id.bgm_forest:
+                setNowBgmId(R.raw.forest);
+                if (mediaPlayer.isPlaying()) playBGM();
+                break;
+            case R.id.bgm_summer_night:
+                setNowBgmId(R.raw.summer_night);
+                if (mediaPlayer.isPlaying()) playBGM();
+                break;
+            case R.id.bgm_beach:
+                setNowBgmId(R.raw.beach);
+                if (mediaPlayer.isPlaying()) playBGM();
+                break;
+            case R.id.bgm_spring_rain:
+                setNowBgmId(R.raw.spring_rain);
+                if (mediaPlayer.isPlaying()) playBGM();
+                break;
+            case R.id.bgm_stove_fire:
+                setNowBgmId(R.raw.stove_fire);
+                if (mediaPlayer.isPlaying()) playBGM();
                 break;
         }
         drawable_layout.closeDrawer(Gravity.LEFT);
