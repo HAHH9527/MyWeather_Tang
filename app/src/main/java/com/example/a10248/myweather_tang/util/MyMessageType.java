32f22c2786715397854fa01ad7dc19d528a937e2
9527Number <1024830255@qq.com>
2018-07-05 20:35:19 +0800

    0705 添加一些消息类型

--- app/src/main/java/com/example/a10248/myweather_tang/util/MyMessageType.java
+++ app/src/main/java/com/example/a10248/myweather_tang/util/MyMessageType.java
@@ -15,4 +15,11 @@ public class MyMessageType {
     public static final int Return_WeatherNow_Message_Fail = 5;
     public static final int Return_WeatherForecast_Message = 6;
     public static final int Return_WeatherForecast_Message_Fail = 7;
+    public static final int Return_Air_Message = 8;
+    public static final int Return_Air_Message_Fail = 9;
+
+    public static final int Login_success = 10;
+    public static final int Login_failed = 11;
+    public static final int Register_success = 12;
+    public static final int Register_failed = 13;
 }
