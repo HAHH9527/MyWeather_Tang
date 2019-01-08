805566859c9f36f27fdba88a00a0898ad7b0f1d8
Tang <1024830255@qq.com>
2019-01-08 12:37:09 +0800

    update import

--- app/src/main/java/com/example/a10248/myweather_tang/thread/LoginThread.java
+++ app/src/main/java/com/example/a10248/myweather_tang/thread/LoginThread.java
@@ -11,12 +11,6 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 public class LoginThread implements Runnable {
 
     private User user;
